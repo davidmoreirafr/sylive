957aab57f321e58b19ea696ca56fd409ee2d3dd6
David Moreira <david@numericore.com>
2018-01-09 19:33:01 +0100

    Some cleaning.

--- src/sylive.cc
+++ src/sylive.cc
@@ -12,38 +12,44 @@
 #include <boost/lexical_cast.hpp>
 
 #include <listen.hh>
+#include <view.hh>
 
-int do_display(imsgbuf *); // FIXME
+void
+close_std() {
+  close(0);
+  close(1);
+  close(2);
+}
 
 int main(int argc, char *argv[])
 {
   if (pledge("stdio inet proc rpath tty", NULL))
     err(2, "pledge");
+
   if (argc == 3) {
     int imsg_fds[2];
 
     if (socketpair(AF_UNIX, SOCK_STREAM, PF_UNSPEC, imsg_fds) == -1)
       err(2, "socketpair");
+
     switch (fork()) {
     case -1:
       err(1, "fork");
     case 0:
       if (pledge("stdio rpath tty", NULL))
       	err(2, "pledge");
+
       close(imsg_fds[0]);
       imsgbuf child_ibuf;
       imsg_init(&child_ibuf, imsg_fds[1]);
-      std::cout << std::fixed << std::setprecision(1);
+
       std::exit(do_display(&child_ibuf));
     }
 
     close(imsg_fds[1]);
-    close(0);
-    close(1);
-    close(2);
+    close_std();
     imsgbuf parent_ibuf;
     imsg_init(&parent_ibuf, imsg_fds[0]);
-
     {
       const int sockfd = do_connect(argv[1], boost::lexical_cast<short>(argv[2]));
       if (pledge("stdio", NULL))
