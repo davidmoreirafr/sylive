28ea8b4b98e2e4c1bae085ddb3eba0c8f60cb1dd
David Moreira <david@numericore.com>
2018-01-11 10:32:41 +0100

    Delete display.cc

--- src/view.cc
+++ src/view.cc
@@ -25,6 +25,22 @@
 #include <fun.hh>
 #include <utils.hh>
 
+void
+tell_user(int line, Placement placement, std::string const& value, imsgbuf *user_ibuf) {
+  Line l;
+  l.line = line;
+  l.placement = placement;
+  strncpy(l.content, value.c_str(), sizeof l.content);
+  imsg_compose(user_ibuf, IMSG_LINE, 0, 0, -1, &l, sizeof l);
+  while (true) {
+    int write = msgbuf_write(&user_ibuf->w);
+    if (write == -1 && errno != EAGAIN)
+      err(1, "msgbuf_write");
+    if (write == 0)
+      break;
+  }
+}
+
 #define BUF_SIZE 1024
 char l[BUF_SIZE];
 
